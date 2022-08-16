c7d45affdadaa540af883e1251b4fb652bebe753
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-16 20:06:58 +0530

    UC1 max Integer

--- GenericDay13/Program.cs
+++ GenericDay13/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GenericDay13
+{
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            bool check = true;
+            while(check)
+            {
+                Console.WriteLine("Select option:\n 1.To find find maximum integer \n 2. To find max string \n 3. to find max float");
+                int option = Int32.Parse(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1:
+                        FindMax obj = new FindMax();
+                        int result = obj.FindMaxInteger(20, 30, 25);
+                        Console.WriteLine(result+" is maxmium");
+                        break;
+                    default:
+                        check = false;
+                        break;
+                }
+            }
+        }
+    }
+}
